d2ac4faa627e4578b0e5ca0b44bb94e214f861d0
Artem Senin <artem.senin@nordigy.ru>
2020-01-25 00:55:46 +0300

    feat: add auto pdf generation in build

---
+++ pdf-gen/index.js
@@ -0,0 +1,31 @@
+const PORT = 3000;
+const fs = require('fs');
+const path = require('path');
+const puppeteer = require('puppeteer');
+
+async function generatePDF() {
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
+  await page.goto(`http://localhost:${PORT}`, {
+    waitUntil: ["load","networkidle0"]
+  });
+  await page.addStyleTag({ content: 'a[href^="http"]:after { display: none !important; }' });
+
+  const pdfBuffer = await page.pdf({ format: 'A4', printBackground: true, margin: {
+      left: '0px',
+      top: '0px',
+      right: '0px',
+      bottom: '0px'
+    } });
+
+  await browser.close();
+  return pdfBuffer;
+}
+
+async function savePDF() {
+  return fs.writeFile(path.resolve(__dirname, '../public/Artem Senin, Front-end developer.pdf'), await generatePDF(), () => {
+  });
+}
+
+savePDF().then(() => console.log('Successfully saved')).catch(err => console.error(err));
+
